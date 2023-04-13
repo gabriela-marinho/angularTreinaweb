baa8126815c0556329a7414ff8052ac6cfb98819
@GabrielaBMarinho <g.barreto.marinho@gmail.com>
2023-04-13 09:35:19 -0300

    validando formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppComponent } from './app.component';
 import { TemplateDrivenComponent } from './components/template-driven/template-driven.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { FormulariosReativosComponent } from './components/formularios-reativos/formularios-reativos.component';
-import { TextAreaCounterComponent } from './components/text-area-counter/text-area-counter.component';
+import {FormulariosReativosComponent} from "./components/formularios-reativos/formularios-reativos.component";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateDrivenComponent,
-    FormulariosReativosComponent,
-    TextAreaCounterComponent
+    FormulariosReativosComponent
+
   ],
   imports: [
     BrowserModule,
